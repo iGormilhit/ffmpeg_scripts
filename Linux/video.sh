a1b5eedac758b7eecb67033c11d319349fffacc7
Alexandre Racine <alexandre.racine1@gmail.com>
2016-06-08 21:53:08 +0200

    path modifications in video.sh

--- Linux/video.sh
+++ Linux/video.sh
@@ -4,13 +4,13 @@
 #For other parameters, or grab video on other systems, consult the official ffmpeg documentation for linux https://trac.ffmpeg.org/wiki/Capture/Desktop
 #At the end of the command adapt your path system where you want to save the file. It should be in the same folder where the previous audio file is saved.
 
-	ffmpeg  -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 -c:v libx264 -qp 0 -preset ultrafast /Users/alexandreracine/Desktop/test/capture.mp4
+	ffmpeg  -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 -c:v libx264 -qp 0 -preset ultrafast /home/alex/Bureau/test/capture.mp4
 
 #Adapt your path system where the first video file is saved above.
 #Convert the video with libvpx codec with .webm container to play the video in several browsers.
 #Convert the video with libxvid codec and .mov container + "-qscale:v 3" parameter (keep a good quality image) to play the video in safari.
 
-	for file in /Users/alexandreracine/Desktop/test/*.mp4; do
+	for file in /home/alex/Bureau/test/*.mp4; do
   	  ffmpeg -i "$file" -c:v libvpx -crf 18 -b:v 0 "${file%.mp4}".webm;
   	  ffmpeg -i "$file" -c:v libxvid -qscale:v 3 "${file%.mp4}".mov;
 	done
