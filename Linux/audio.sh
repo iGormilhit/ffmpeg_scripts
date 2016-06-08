bb5172a5d5ff3a0bb0eda37c28e9db332c80f2e8
Alexandre Racine <alexandre.racine1@gmail.com>
2016-06-09 00:32:24 +0200

    modified comments

--- Linux/audio.sh
+++ Linux/audio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#This command must be adapted according to your os. "-f alsa -i hw:0" is for Linux. Consult ffmpeg documentation to grab audio on other systems. Codec audio aac +parameters for lossless recording.
+#This command must be adapted according to your os. "-f alsa -i hw:0" is for Linux. "hw" could be "0" or "1" depending on your system. Type "arecord -L" to put the number 
+#related to your system. Consult ffmpeg documentation for more options or to grab audio on other systems. Codec audio aac +parameters for lossless recording.
 #At the end of the command adapt your path system where you want to save the file.
 #It is recommended to create a folder dedicated to the multiple video files you will obtain. To avoid modifying other media files you have on your computer.
 
--- OSX/audio.sh
+++ OSX/audio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#This command must be adapted according to your os. -f avfoundation -i +parameter is for os x. Consult ffmpeg documentation to grab audio on other systems.
+#This command must be adapted according to your os. "-f avfoundation -i +parameter" is for os x. Type `ffmpeg -f avfoundation -list_devices true -i ""` to know which audio
+#device to record acording to your system. Then put the number "0, 1 or n". Consult ffmpeg documentation to know more options or to grab audio on other systems.
 #At the end of the command adapt your path system where you want to save the file.
-#It is recommended to create a folder dedicated to the multiple video files you will obtain. To avoid modifying other media files you have in your computer.
+#It is recommended to create a folder dedicated to the multiple video files you will obtain. To avoid modifying other media files you have on your computer.
 
 	ffmpeg -f avfoundation -i ":1" -c:a aac -qp 0 -preset ultrafast /path/to/file/test/audio.aac
 
