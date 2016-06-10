f34213b96a7c15a555e45c4ff3cea1569f9f6732
Alexandre Racine <alexandre.racine1@gmail.com>
2016-06-10 13:31:44 +0200

    improved scripts with variable for path

--- OSX/cut.sh
+++ OSX/cut.sh
@@ -6,13 +6,14 @@
 #and rename the file as audio_cut.aac
 #watch to have the same time for the audio & video file to synchronize
 
-	ffmpeg -i /path/to/file/test/audio.aac -ss 00:00:01 -t 00:00:10 -c:a copy /path/to/file/test/audio_cut.aac
+	path="/path/to/save/file/"
+	ffmpeg -i /path/to/file/test/audio.aac -ss 00:00:01 -t 00:00:10 -c:a copy "${path%}"audio_cut.aac
 
 #For every ".mp4" file, cut the video from the time [HH:MM:SS] for a duration of [HH:MM:SS] eg: from second 1 until second 28 put 00:00:01 -t 00:00:27
 #and convert in ".webm" and ".mov" files while adding "_cut" in the name
 #adapt the 2 time values according to your needs.
 	
-	for name in /path/to/file/test/*.mp4; do
+	for name in "${path%}"*.mp4; do
 	ffmpeg -i "$name" -ss 00:00:01 -t 00:00:10 -async 1 -strict -2 "${name%.mp4}"_cut.mp4;
 	ffmpeg -i "$name" -ss 00:00:01 -t 00:00:10 -c:v libxvid -qscale:v 3 -async 1 -strict -2 "${name%.mov}"_cut.mov;
 	ffmpeg -i "$name" -ss 00:00:01 -t 00:00:10 -c:v libvpx -crf 18 -c:a libvorbis -b:v 0 -async 1 -strict -2 "${name%.webm}"_cut.webm;
@@ -23,14 +24,14 @@
 #Compile the cut audio & video files. Watch to have the audio & video files synchronized with the same duration
 #rename with "_all" at the end
 
-	for file in /path/to/file/test/*_cut.mp4; do
-          ffmpeg -i "$file" -i /path/to/file/test/audio_cut.aac -strict experimental capture_cut_all.mp4;
+	for file in "${path%}"*_cut.mp4; do
+          ffmpeg -i "$file" -i "${path%}"audio_cut.aac -strict experimental capture_cut_all.mp4;
         done
-        for file in /path/to/file/test/*_cut.mov; do
-          ffmpeg -i "$file" -i /path/to/file/test/audio_cut.aac -c:v libxvid -c:a aac -qscale:v 3 -strict experimental capture_cut_all.mov;
+        for file in "${path%}"*_cut.mov; do
+          ffmpeg -i "$file" -i "${path%}"audio_cut.aac -c:v libxvid -c:a aac -qscale:v 3 -strict experimental capture_cut_all.mov;
         done
-        for file in /path/to/file/test/*_cut.webm; do
-          ffmpeg -i "$file" -i /path/to/file/test/audio_cut.aac -c:v libvpx -crf 18 -b:v 0 -c:a libvorbis -strict experimental capture_cut_all.webm;
+        for file in "${path%}"*_cut.webm; do
+          ffmpeg -i "$file" -i "${path%}"audio_cut.aac -c:v libvpx -crf 18 -b:v 0 -c:a libvorbis -strict experimental capture_cut_all.webm;
         done
 
 	echo "successfully merged and converted"
@@ -41,14 +42,14 @@
 	read name
 	if [ -n $name ];
 	then
-  	mv /path/to/file/test/*_all.mp4 /path/to/file/test/${name}.mp4
-	mv /path/to/file/test/*_all.webm /path/to/file/test/${name}.webm
-	mv /path/to/file/test/*_all.mov /path/to/file/test/${name}.mov
+  	mv "${path%}"*_all.mp4 "${path%}"/${name}.mp4
+	mv "${path%}"*_all.webm "${path%}"/${name}.webm
+	mv "${path%}"*_all.mov "${path%}"/${name}.mov
 
 	echo "successfully renamed"
 fi
 
 #Remove all "_cut" files and keep the original audio and video files + the final files.
-	rm /path/to/file/test/*_cut.*
+	rm "${path%}"*_cut.*
 
 exit 0
--- OSX/video.sh
+++ OSX/video.sh
@@ -3,6 +3,7 @@
 #This command must be adapted according to your os. From  "-f avfoundation" until "1" it is for os x. Consult ffmpeg documentation to grab video on other systems.
 #At the end of the command adapt your path system where you want to save the file. It should be in the same folder where the previous audio file is saved.
 
-	ffmpeg -f avfoundation -capture_cursor 1 -capture_mouse_clicks 1 -i "1" -c:v libx264 -qp 0 -preset ultrafast /path/to/file/test/capture.mp4
+	path="/path/to/save/file/"
+	ffmpeg -f avfoundation -capture_cursor 1 -capture_mouse_clicks 1 -i "1" -c:v libx264 -qp 0 -preset ultrafast "${path%}"capture.mp4
 
 exit 0
