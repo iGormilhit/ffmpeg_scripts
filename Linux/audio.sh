eeeb50bb83505dfe44e3bbd8e0132670a450a90a
iGor milhit <igor@milhit.ch>
2016-06-16 15:43:42 +0200

    1. Implémentation help     2. Possibilité pour la suite :        * Unifier le code pour linux et osx        * avec un test genre :        * https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux

--- Linux/audio.sh
+++ Linux/audio.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
+# -*- coding: utf8 -*-
+#
+# This script is ...
+#
+# [Licence] [date]
+# Contributors: [name]<email>
 
-#This command must be adapted according to your os. "-f alsa -i hw:0" is for Linux. "hw" could be "0" or "1" depending on your system. Type "arecord -L" to put the number 
-#related to your system. Consult ffmpeg documentation for more options or to grab audio on other systems. Codec audio aac +parameters for lossless recording.
-#At the end of the command adapt your path system where you want to save the file.
-#It is recommended to create a folder dedicated to the multiple video files you will obtain. To avoid modifying other media files you have on your computer.
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 your/path name-of-file";
-    echo "Use q to stop";
-    exit 1;
+# help function
+help() {
+    echo
+    echo "$0 use ffmpeg to record the audio input"
+    echo
+    echo "-h or --help display this message"
+    echo
+    echo "Usage : $0 path/to/your/files name-of-your-file card-number"
+    echo
+    echo "Linux users will find the soundcard number with:"
+    echo "cat /proc/asound/cards"
+    echo
+    echo "Consult ffmpeg documentation for more options"
+    echo "or to grab audio on other systems:"
+    echo "https://www.ffmpeg.org/documentation.html"
+    echo
+    echo "Codec audio aac +parameters for lossless recording"
+
+    exit 1
+}
+
+
+# display help message when the necessary arguments aren't there
+if [ $# -lt 3 ]; then
+    help
 fi
+
+# display help message when -h options is used
+while test -n "$1"; do
+    case "$1" in
+        --help|-h)
+            help
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            help
+            ;;
+    esac
+    shift
+done
 
 path=$1
 name=$2
+soundcardnumber=$3
 
-ffmpeg -f alsa -i hw:0 -c:a aac -qp 0 -preset ultrafast $path/$name.aac
+ffmpeg -f alsa -i hw:$soundcardnumber -c:a aac -qp 0 -preset ultrafast $path/$name.aac
 echo "audio stream successfully recorded"
 
 exit 0
