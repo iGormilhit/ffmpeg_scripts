f439c6df7ae8fe3d9c9dcea711d390f78f47b0ce
iGor milhit <igor@milhit.ch>
2016-06-16 17:25:12 +0200

    Start of the OS test

--- Linux/audio.sh
+++ Linux/audio.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # -*- coding: utf8 -*-
 #
-# This script is ...
+# This script is [doing what ?]
 #
 # [Licence] [date]
 # Contributors: [name]<email>
@@ -17,7 +17,10 @@ help() {
     echo "Usage : $0 path/to/your/files name-of-your-file card-number"
     echo
     echo "Linux users will find the soundcard number with:"
-    echo "cat /proc/asound/cards"
+    echo "cat /proc/asound/cards or arecord -l"
+    echo
+    echo "OS X users will fin the soundcard number with:"
+    echo "ffmpeg -f avfoundation -list_devices true -i \"\""
     echo
     echo "Consult ffmpeg documentation for more options"
     echo "or to grab audio on other systems:"
@@ -35,24 +38,27 @@ if [ $# -lt 3 ]; then
 fi
 
 # display help message when -h options is used
-while test -n "$1"; do
-    case "$1" in
-        --help|-h)
-            help
-            ;;
-        *)
-            echo "Unknown argument: $1"
-            help
-            ;;
-    esac
-    shift
-done
+# This don't work, it break the $1 $2 and $3
+# while test -n "$1"; do
+#     case "$1" in
+#         --help|-h)
+#             help
+#             ;;
+#     esac
+#     shift
+# done
 
 path=$1
 name=$2
 soundcardnumber=$3
+os=`uname`
 
-ffmpeg -f alsa -i hw:$soundcardnumber -c:a aac -qp 0 -preset ultrafast $path/$name.aac
+if [ $os == "Linux" ]; then
+    ffmpeg -f alsa -i hw:$soundcardnumber -c:a aac -qp 0 -preset ultrafast $path/$name.aac;
+else
+    echo "Misses the OS X command"
+fi
+
 echo "audio stream successfully recorded"
 
 exit 0
